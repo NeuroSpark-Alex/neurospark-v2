3713f8095922132d8e2de6047defef34c621f3ec
NeuroSpark-Alex <monarch.tld@gmail.com>
2026-01-20 00:07:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    // Esto asegura que process.env.API_KEY esté disponible en el navegador
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 });
